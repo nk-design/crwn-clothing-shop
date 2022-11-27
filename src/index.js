5695faed524d3f5f5c74a8072f8f1348a55b06c3
Mykola Kovtun <mykola.kovtun@evolv.ai>
2022-11-27 16:50:44 +0200

    Added saga and stripe

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import {Provider} from "react-redux";
-import { store } from './store/store';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <React.StrictMode>
-      <Provider store={store}>
+import App from "./App";
+import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
-      </Provider>
-    </React.StrictMode>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
